955cb5d7104f9ac65e4e361ebff8574ef694a8b7
s1140524-oss <s1140524@dmhs.kh.edu.tw>
2026-05-04 09:46:45 +0800

    Create logic.cpp

---
+++ logic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a = 8;
+  int b = 3 ;
+  int c = a-b;
+count<<(a<b)<<"";
+count<<(a>=b)<<"";
+count<<(a>=c)<<"";
+count<<(b<c)<<"";
+count<<(b==b)<<"";
+count<<(a!=c)<<"";
+    return 0;
+}
