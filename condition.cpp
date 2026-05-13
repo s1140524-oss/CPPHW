ffa297ca06c63d84bd7da7c19eb9ab9913cebdb5
s1140524-oss <s1140524@dmhs.kh.edu.tw>
2026-05-13 15:14:08 +0800

    Create  condition.cpp

---
+++ condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+   cout<<"請輸入年齡:"
+   cin>>age;
+   
+    if(age>=18) {
+	cout<<"你已經成年,可以壞壞\n"
+	}
+   if(age>=18)
+   cout<<"你已經成年,可以壞壞\n"
+    return 0;
+}
