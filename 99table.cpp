6622e249e77728a2cc1aff7865c6b00a92a1e6ec
s1140524-oss <s1140524@dmhs.kh.edu.tw>
2026-05-25 09:47:50 +0800

    Implement multiplication table in 99table.cpp
    Fix loop initialization and output formatting in multiplication table.

---
+++ 99table.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=; i<= 9; i++){
+       for(int j= 1; j<= 9; j++) {
+	       cout<<i<<" * "<< j<<" = "<< i * j<< "\t";
+	   }
+	}
+    return 0;
+}
